ee29f00237340c0c299c4ff405b6b626164d92c1
Khushi Gupta <khushi.gupta493@gmail.com>
2018-11-27 08:56:18 -0500

    Filled values in ArrayLists
    I filled values in the ArrayLists in the main method to use in my method.

--- GHArrayListPractice.java
+++ GHArrayListPractice.java
@@ -22,7 +22,22 @@ public class GHArrayListPractice{
 
 	public static void main(String[]args){
 
+		ArrayList <Integer> list1 = new ArrayList<>();
+		ArrayList <Integer> list2 = new ArrayList<>();
 
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		list1.add(4);
+		list1.add(5);
+
+		list2.add(6);
+		list2.add(7);
+		list2.add(8);
+		list2.add(9);
+		list2.add(10);
+
+		System.out.println(combineLists(list1, list2));
 	}
 
 }
